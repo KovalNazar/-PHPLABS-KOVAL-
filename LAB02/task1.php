99be18822c3449428fd6cce6562b7cace427a463
KovalNazar <kn1b22.koval@kpnu.edu.ua>
2025-06-22 22:20:01 +0300

    Create task1.php

---
+++ LAB02/task1.php
@@ -0,0 +1,9 @@
+<?php
+// Кількість градусів
+$degrees = 180;
+
+// Формула для перетворення: radians = degrees * (π / 180)
+$radians = $degrees * (pi() / 180);
+
+echo "$degrees градусів = $radians радіан(и)";
+?>
